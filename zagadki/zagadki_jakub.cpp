0602cadb0099b1724c83fe06c632b78472c98c34
sala multimedialna 2 <sala multimedialna 2>
2017-02-27 18:37:43 +0100

    commit Jakuba

--- zagadki/zagadki_jakub.cpp
+++ zagadki/zagadki_jakub.cpp
@@ -1,1 +1,19 @@
+#include "zagadki_Jakub.hpp"
+#include <iostream>
 
+void zag_Jakub()
+{
+    int wynik()
+    cout << "oto moja zagadka" <<endl;
+    std::cout << "oblicz"
+              std::cout << "700+300*6=" <<std::endl;
+    int obliczenie;
+    cin >> obliczenie;
+    if( obliczenie ==2500)
+        std::cout << "jesteœ geniuszem" << std::endl;
+    else
+        std::cout <<"nie jesteœ geniuszem" << std::endl;
+
+}
+
+#endif // zagadki_Jakub
