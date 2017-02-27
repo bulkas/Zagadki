2fa2e9396fee7b3b44c5a1849582d1e0c8bad6ea
8D <you@example.com>
2017-02-27 18:26:36 +0100

    commit oliwiera

--- zagadki/zagadki_oliwier.cpp
+++ zagadki/zagadki_oliwier.cpp
@@ -1,1 +1,12 @@
+#include "zagadki_oliwier.hpp"
+#include <iostream>
+void zag_oliwier(){
+int wynik;
+std::cout << "oblicz:" << std::endl;
+std::cout << "(2+8*6/2)-32=" << std::endl;
+if (wynik >=-2)
+std::cout << "dobry wynik" << std::endl;
+else
+std::cout << "nie siedz przy komputerze, idz ucz sie matematyki" << std::endl;
+}
 
