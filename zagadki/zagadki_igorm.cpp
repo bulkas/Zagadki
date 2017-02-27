660d8cdb28a3d0ac572a78df010b5282902f6423
IGOR MICHALAK <IGOR MICHALAK>
2017-02-27 18:47:11 +0100

    comit igoram

--- zagadki/zagadki_igorm.cpp
+++ zagadki/zagadki_igorm.cpp
@@ -1,1 +1,3 @@
-
+#include "zagadki_igorm.cpp"
+void zagadki_igorm ();
+cout << "to jest moja zagadka" << endl;
--- zagadki/zagadki_igorm.hpp
+++ zagadki/zagadki_igorm.hpp
@@ -1,1 +1,4 @@
-
+#itdef zagadki_igorm_hpp
+#define zagadki_igorm_hpp
+void zagadki_igorm ();
+#edif
