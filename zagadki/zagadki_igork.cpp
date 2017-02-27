6bcefbd43a0914c4667f7f10a889d46ab39a0114
Your Name <you@example.com>
2017-02-27 18:33:36 +0100

    commit igora k main cpp

--- zagadki/zagadki_igork.cpp
+++ zagadki/zagadki_igork.cpp
@@ -1,1 +1,16 @@
+#include "zagadki_igork.hpp"
+#include <iostream>
+void zag_igork()
+{
+    std::cout << "|TO|JEST|MOJA|ZAGADKA|" << std::endl;
+}
+
+
+
+
+
+
+
+
+
 
--- zagadki/zagadki_igork.hpp
+++ zagadki/zagadki_igork.hpp
@@ -1,1 +1,11 @@
+#ifndef zagadki_igork_hpp
+#define zagadki_igork_hpp
+void zag_igork();
 
+
+
+
+
+
+
+#endif
