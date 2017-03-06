e0420174dc3e6e99d71f2c2d1177624f42dca506
Your Name <you@example.com>
2017-03-06 17:43:40 +0100

    COMMIT IGORA K dodałem swoja zagadke

--- zagadki/zagadki_igork.cpp
+++ zagadki/zagadki_igork.cpp
@@ -2,8 +2,30 @@
 #include <iostream>
 void zag_igork()
 {
+    float liczba;
+
     std::cout << "|TO|JEST|MOJA|ZAGADKA|" << std::endl;
+    std::cout << "          |           " << std::endl;
+    std::cout << "          |           " << std::endl;
+    std::cout << " m³otek i gwóŸdŸ kosztuj¹ 1,1 dolara,  " << std::endl;
+    std::cout << " a m³otek kosztuje o jednego dolara wiêcej od gwoŸdzia.  " << std::endl;
+    std::cout << " Ile kosztuje gwóŸdŸ? " << std::endl;
+    std::cout << " ODPOWEIDZ " << std:: endl;
+    std::cin >> liczba;
+    if ( liczba == 0,05)
+    {
+        std::cout << " DOBRZE !!! " << std::endl;
+    }
+
+    else
+    {
+        std::cout << " ZLE !!! " << std:: endl;
+    }
 }
+
+
+
+
 
 
 
