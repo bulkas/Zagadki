44fd7d24934e986480b316f6007feffa7c62cb0e
8D <you@example.com>
2017-03-06 18:52:21 +0100

    3 poprawa commitu oliwiera

--- zagadki/zagadki_oliwier.cpp
+++ zagadki/zagadki_oliwier.cpp
@@ -1,13 +1,28 @@
 #include "zagadki_oliwier.hpp"
 #include <iostream>
-void zag_oliwier(){
-int wynik;
-std::cout << "oblicz:" << std::endl;
-std::cout << "(2+8*6/2)-32=" <<std::endl;
-std::cin >> wynik;
-if (wynik >=-2)
-std::cout << "dobry wynik" << std::endl;
-else
-std::cout << "niestety zly wynik" << std::endl;
-}
+void zag_oliwier()
+{
+    int wynik;
+    std::cout << "oblicz:" << std::endl;
+    std::cout << "(2+8*6/2)-32=" <<std::endl;
+    std::cin >> wynik;
+    if (wynik ==-2)
+    {
+        std::cout << "dobry wynik" << std::endl;
+    }
+    else
+    {
+        std::cout << "niestety zly wynik" << std::endl;
+        while (wynik != -2)
+        {
+            std::cout << "sproboj jeszcze raz" << std::endl;
+            std::cout << "(2+8*6/2)-32=" <<std::endl;
+            std::cin >> wynik;
 
+        }
+        if (wynik ==-2)
+        {
+            std::cout << "dobry wynik" << std::endl;
+        }
+    }
+}
