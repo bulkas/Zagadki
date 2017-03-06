bbee8b83ca9aec1971417a630980c73eba01ce2a
8D <you@example.com>
2017-03-06 17:55:08 +0100

    poprawa commitu oliwiera

--- zagadki/zagadki_oliwier.cpp
+++ zagadki/zagadki_oliwier.cpp
@@ -3,10 +3,10 @@
 void zag_oliwier(){
 int wynik;
 std::cout << "oblicz:" << std::endl;
-std::cout << "(2+8*6/2)-32=" << std::endl;
+std::cout << "(2+8*6/2)-32=" << wynik << std::endl;
 if (wynik >=-2)
 std::cout << "dobry wynik" << std::endl;
 else
-std::cout << "nie siedz przy komputerze, idz ucz sie matematyki" << std::endl;
+std::cout << "niestety zly wynik" << std::endl;
 }
 
