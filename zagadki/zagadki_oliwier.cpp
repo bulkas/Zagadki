276c67a90fb2a0c1682182172983d9553445570c
8D <you@example.com>
2017-03-06 18:02:28 +0100

    2 zmiany pliku oliwiera

--- zagadki/zagadki_oliwier.cpp
+++ zagadki/zagadki_oliwier.cpp
@@ -3,7 +3,8 @@
 void zag_oliwier(){
 int wynik;
 std::cout << "oblicz:" << std::endl;
-std::cout << "(2+8*6/2)-32=" << wynik << std::endl;
+std::cout << "(2+8*6/2)-32=" <<std::endl;
+std::cin >> wynik;
 if (wynik >=-2)
 std::cout << "dobry wynik" << std::endl;
 else
